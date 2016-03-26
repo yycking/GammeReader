618ca2b0ceb981ed49fa5b7b5b04532c454bb0c4
YehYungCheng <yehyungcheng@yungchengtekimacbook-air.local>
2016-03-26 17:11:23 +0800

    Initial Commit

---
+++ GammeReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GammeReader
+//
+//  Created by YehYungCheng on 2016/3/26.
+//  Copyright © 2016年 YehYungCheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
